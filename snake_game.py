670a31359168ebfb5a2627bffbacf8084eec1b10
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-02 17:56:25 +0800

    create the game window

---
+++ snake_game.py
@@ -0,0 +1,16 @@
+import tkinter
+import random
+
+ROWS = 25
+COLS = 25
+TILE_SIZE = 25
+
+WINDOW_WIDTH = TILE_SIZE * COLS
+WINDOW_HEIGHT = TILE_SIZE * ROWS
+
+#create the game window
+window = tkinter.Tk()
+window.title('Snake Game')
+window.resizable(False, False)
+
+window.mainloop()
