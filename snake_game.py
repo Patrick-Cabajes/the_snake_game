556e2751e09fdc83ea198c6c9fdbbac63bcc31d4
Patrick_Cabajes <patrickcabajes07@gmail.com>
2024-10-04 14:03:28 +0800

    finally created the snake's head

--- snake_game.py
+++ snake_game.py
@@ -8,6 +8,11 @@ TILE_SIZE = 25
 WINDOW_WIDTH = TILE_SIZE * COLS
 WINDOW_HEIGHT = TILE_SIZE * ROWS
 
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 #create the game window
 window = tkinter.Tk()
 window.title('Snake Game')
@@ -26,6 +31,20 @@ screen_height = window.winfo_screenheight()
 window_x = int((screen_width/2) - (window_width/2))
 window_y = int((screen_height/2) - (window_height/2))
 
+#format "(w)x(h)+(x)+(y)"
 window.geometry(f'{window_width}x{window_height}+{window_x}+{window_y}')
+
+#initialize the game
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) #single tile, snake's head
+
+def draw():
+    global snake
+
+    #draw snake
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "pink")
+
+    window.after(100, draw) #call draw again every 100ms (1/10 of a second) = 10 frames/second
+
+draw()
 
 window.mainloop()
